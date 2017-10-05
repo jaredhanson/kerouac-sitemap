f67b8e6700a98a20e86a52f1972cba17b0eeb65b
Jared Hanson <jaredhanson@gmail.com>
2017-10-04 18:51:11 -0700

    More tests.

--- test/urlset.test.js
+++ test/urlset.test.js
@@ -43,7 +43,7 @@ describe('urlset', function() {
     });
   }); // with one page
   
-  describe('with one page', function() {
+  describe('with two pages', function() {
     var page, err;
 
     before(function(done) {
@@ -115,6 +115,78 @@ describe('urlset', function() {
       expect(page.body).to.equal(expected);
     });
   }); // with site containing /robots.txt
+  
+  describe('with site containing other sitemaps', function() {
+    var page, err;
+
+    before(function(done) {
+      chai.kerouac.use(sitemap())
+        .page(function(page) {
+          page.site = new mock.Site();
+          page.site.set('base url', 'http://www.example.com/');
+          page.pages = [
+            { url: '/hello' },
+            { url: '/stores/store1_sitemap.xml', sitemap: true },
+            { url: '/stores/store2_sitemap.xml', sitemap: true },
+            { url: '/stores/store3_sitemap.xml', sitemap: true }
+          ];
+        })
+        .end(function(p) {
+          page = p;
+          done();
+        })
+        .dispatch();
+    });
+  
+    it('should write sitemap.xml', function() {
+      var expected = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        '  <url>',
+        '    <loc>http://www.example.com/hello</loc>',
+        '  </url>',
+        '</urlset>',
+        ''
+      ].join("\n");
+      
+      expect(page.body).to.equal(expected);
+    });
+  }); // with site containing other sitemaps
+  
+  describe('with site containing a sitemap index', function() {
+    var page, err;
+
+    before(function(done) {
+      chai.kerouac.use(sitemap())
+        .page(function(page) {
+          page.site = new mock.Site();
+          page.site.set('base url', 'http://www.example.com/');
+          page.pages = [
+            { url: '/hello' },
+            { url: '/sitemap_index.xml', sitemapIndex: true }
+          ];
+        })
+        .end(function(p) {
+          page = p;
+          done();
+        })
+        .dispatch();
+    });
+  
+    it('should write sitemap.xml', function() {
+      var expected = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        '  <url>',
+        '    <loc>http://www.example.com/hello</loc>',
+        '  </url>',
+        '</urlset>',
+        ''
+      ].join("\n");
+      
+      expect(page.body).to.equal(expected);
+    });
+  }); // with site containing a sitemap index
   
   /*
   
