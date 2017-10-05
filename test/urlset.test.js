c11626ee30284a45e5a43903718adf3ab8ce01d9
Jared Hanson <jaredhanson@gmail.com>
2017-10-04 18:46:26 -0700

    More tests.

--- test/urlset.test.js
+++ test/urlset.test.js
@@ -3,43 +3,13 @@ var mock = require('chai-kerouac-middleware');
 var sitemap = require('../lib');
 
 
-function MockSite() {
-  this.settings = {};
-  this.pages = {};
-}
-
-MockSite.prototype.get =
-MockSite.prototype.set = function(setting, val) {
-  if (1 == arguments.length) {
-    return this.settings[setting];
-  } else {
-    this.settings[setting] = val;
-    return this;
-  }
-}
-
-MockSite.prototype.page = function(path, fn) {
-  this.pages[path] = new MockPage(path, fn);
-}
-
-function MockPage(path, fn) {
-  this.path = path;
-  this.fn = fn;
-  this.data = '';
-}
-
-MockPage.prototype.write = function(data) {
-  this.data += data;
-}
-
-
-describe('sitemap plugin', function() {
+describe('urlset', function() {
   
   it('should export function', function() {
     expect(sitemap).to.be.a('function');
   });
   
-  describe('default exclusion', function() {
+  describe('with one page', function() {
     var page, err;
 
     before(function(done) {
@@ -48,6 +18,41 @@ describe('sitemap plugin', function() {
           page.site = new mock.Site();
           page.site.set('base url', 'http://www.example.com/');
           page.pages = [
+            { url: '/' }
+          ];
+        })
+        .end(function(p) {
+          page = p;
+          done();
+        })
+        .dispatch();
+    });
+  
+    it('should write sitemap.xml', function() {
+      var expected = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        '  <url>',
+        '    <loc>http://www.example.com/</loc>',
+        '  </url>',
+        '</urlset>',
+        ''
+      ].join("\n");
+      
+      expect(page.body).to.equal(expected);
+    });
+  }); // with one page
+  
+  describe('with one page', function() {
+    var page, err;
+
+    before(function(done) {
+      chai.kerouac.use(sitemap())
+        .page(function(page) {
+          page.site = new mock.Site();
+          page.site.set('base url', 'http://www.example.com/');
+          page.pages = [
+            { url: '/' },
             { url: '/contact/' }
           ];
         })
@@ -63,6 +68,9 @@ describe('sitemap plugin', function() {
         '<?xml version="1.0" encoding="UTF-8"?>',
         '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
         '  <url>',
+        '    <loc>http://www.example.com/</loc>',
+        '  </url>',
+        '  <url>',
         '    <loc>http://www.example.com/contact/</loc>',
         '  </url>',
         '</urlset>',
@@ -71,87 +79,44 @@ describe('sitemap plugin', function() {
       
       expect(page.body).to.equal(expected);
     });
-  }); // default exclusion
+  }); // with two pages
+  
+  describe('with site containing /robots.txt', function() {
+    var page, err;
+
+    before(function(done) {
+      chai.kerouac.use(sitemap())
+        .page(function(page) {
+          page.site = new mock.Site();
+          page.site.set('base url', 'http://www.example.com/');
+          page.pages = [
+            { url: '/robots.txt' },
+            { url: '/hello' },
+          ];
+        })
+        .end(function(p) {
+          page = p;
+          done();
+        })
+        .dispatch();
+    });
+  
+    it('should write sitemap.xml', function() {
+      var expected = [
+        '<?xml version="1.0" encoding="UTF-8"?>',
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
+        '  <url>',
+        '    <loc>http://www.example.com/hello</loc>',
+        '  </url>',
+        '</urlset>',
+        ''
+      ].join("\n");
+      
+      expect(page.body).to.equal(expected);
+    });
+  }); // with site containing /robots.txt
   
   /*
-  describe('when invoked on a site with two pages', function() {
-    var site = new MockSite();
-    site.set('base url', 'http://www.example.com/')
-    site.page('/hello.html', function(){});
-    site.page('/company/contact.html', function(){});
-    
-    sitemap()(site, site.pages);
-    
-    it('should add /sitemap.xml page', function() {
-      expect(site.pages).to.include.keys('/sitemap.xml');
-    });
-    
-    describe('and then rendering sitemap.xml', function() {
-      var p = site.pages['/sitemap.xml'];
-
-      it('should write .htaccess', function(done) {
-        var expected = [
-          '<?xml version="1.0" encoding="UTF-8"?>',
-          '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-          '  <url>',
-          '    <loc>http://www.example.com/company/contact.html</loc>',
-          '  </url>',
-          '  <url>',
-          '    <loc>http://www.example.com/hello.html</loc>',
-          '  </url>',
-          '</urlset>',
-          ''
-        ].join("\n");
-        
-        p.end = function() {
-          expect(p.data).to.equal(expected);
-          done();
-        };
-        
-        p.fn(p, function(err) {
-          return done(new Error('should not call next'));
-        });
-      });
-    });
-  });
-  
-  describe('when invoked on a site with a robots.txt file', function() {
-    var site = new MockSite();
-    site.set('base url', 'http://www.example.com/')
-    site.page('/hello.html', function(){});
-    site.page('/robots.txt', function(){});
-    
-    sitemap()(site, site.pages);
-    
-    it('should add /sitemap.xml page', function() {
-      expect(site.pages).to.include.keys('/sitemap.xml');
-    });
-    
-    describe('and then rendering sitemap.xml', function() {
-      var p = site.pages['/sitemap.xml'];
-
-      it('should write .htaccess', function(done) {
-        var expected = [
-          '<?xml version="1.0" encoding="UTF-8"?>',
-          '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
-          '  <url>',
-          '    <loc>http://www.example.com/hello.html</loc>',
-          '  </url>',
-          '</urlset>',
-          ''
-        ].join("\n");
-        
-        p.end = function() {
-          expect(p.data).to.equal(expected);
-          done();
-        };
-        
-        p.fn(p, function(err) {
-          return done(new Error('should not call next'));
-        });
-      });
-    });
-  });
   
   describe('when invoked on a site with CSS and JavaScript', function() {
     var site = new MockSite();
