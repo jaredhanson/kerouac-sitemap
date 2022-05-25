5c2f2ba9898de2c9e77962f5cb42210420aa0c3c
Jared Hanson <jaredhanson@gmail.com>
2022-05-25 07:00:36 -0700

    Add tests.

--- test/mapper.test.js
+++ test/mapper.test.js
@@ -4,4 +4,16 @@ var sitemap = require('../lib');
 
 describe('Mapper', function() {
   
+  it('should request sitemap', function(done) {
+    chai.kerouac.map(sitemap.createMapper())
+      .close(function() {
+        expect(this).to.request([ '/sitemap.xml' ]);
+        expect(this.pages['/sitemap.xml'].locals).to.deep.equal({
+          pages: []
+        })
+        done();
+      })
+      .generate();
+  }); // should request sitemap
+  
 });
